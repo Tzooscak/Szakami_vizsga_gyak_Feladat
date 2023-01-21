f2cf0b3904b5ab2ecf0077ca5ef03d66284ba3b7
Dr.Senkiházi <89943379+Tzooscak@users.noreply.github.com>
2023-01-21 11:58:33 +0100

    2.feladat kész

--- Szakma_program/src/main/java/com/mycompany/szakma_program/versenyzok.java
+++ Szakma_program/src/main/java/com/mycompany/szakma_program/versenyzok.java
@@ -8,8 +8,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 /**
  *
@@ -23,6 +26,18 @@ public class versenyzok extends javax.swing.JFrame {
     public versenyzok() {
         initComponents();
         Tablatolt(tabla);
+        
+        TableRowSorter<TableModel> sorter = new TableRowSorter<>(tabla.getModel());
+        tabla.setRowSorter(sorter);
+        ArrayList<RowSorter.SortKey> sortKeys = new ArrayList<>();
+        int columnIndexForJob = 2;
+        sortKeys.add(new RowSorter.SortKey(columnIndexForJob, SortOrder.DESCENDING));
+ 
+        int columnIndexForName = 3;
+        sortKeys.add(new RowSorter.SortKey(columnIndexForName, SortOrder.DESCENDING));
+        sorter.setSortKeys(sortKeys);
+        sorter.sort();
+
     }
 
     
@@ -32,6 +47,7 @@ public class versenyzok extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tabla = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -46,26 +62,65 @@ public class versenyzok extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tabla);
 
+        jButton1.setText("Törlés");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(19, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton1)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(110, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(19, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/vizsga_szakma", "root", "");
+            Statement smt = con.createStatement();
+            DefaultTableModel model = (DefaultTableModel) tabla.getModel();
+            int sstam = tabla.getSelectedRow();
+            System.out.println(sstam);
+            int ertek = Integer.parseInt(String.valueOf(tabla.getValueAt(sstam, 0)));
+            smt.executeUpdate("DELETE FROM versenyzo WHERE versenyzo.id = '" + ertek + "'");
+            Tablatorol(tabla);
+            Tablatolt(tabla);
+            con.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    public static void Tablatorol(JTable JTable) {
+        DefaultTableModel model = (DefaultTableModel) JTable.getModel();
+        int sorszam = model.getRowCount();
+        for (int i = 0; i < sorszam; i++) {
+            model.removeRow(0);
+        }
+    }
+    
     public static void Tablatolt(JTable JTable) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -123,6 +178,7 @@ public class versenyzok extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tabla;
     // End of variables declaration//GEN-END:variables
